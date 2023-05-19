376f0552e7fe8b2a02a6f39432d4616e59c7f0d4
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2023-05-19 11:00:58 +0300

    updated imports

--- backend/get_products.py
+++ backend/get_products.py
@@ -1,8 +1,8 @@
-import information.products
+import backend.information.products
 
 
 def get_product_names() -> list:
-    return [item for item in dir(information.products) if
+    return [item for item in dir(backend.information.products) if
             not item.startswith("__") and item not in ['Product', 'factories']]
 
 
@@ -10,10 +10,10 @@ def get_products() -> dict:
     names = get_product_names()
     prod = dict()
     for product_name in names:
-        prod[product_name] = eval(f'information.products.{product_name}')
+        prod[product_name] = eval(f'backend.information.products.{product_name}')
 
     return prod
 
 
 if __name__ == "__main__":
-    pass
+    print(get_products())
