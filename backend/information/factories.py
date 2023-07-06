8ff9911ffd5294008667c6612064eea188b7a5b7
ChainsAre2Tight <124983704+ChainsAre2Tight@users.noreply.github.com>
2023-07-06 15:26:20 +0300

    added some recipes for satisfactory

--- backend/information/factories.py
+++ backend/information/factories.py
@@ -4,3 +4,9 @@ crop_farm = Factory('Crop farm')
 water_pump = Factory('Water pump')
 food_factory = Factory('Food factory')
 livestock_farm = Factory('Livestock farm')
+
+# satisfactory
+assembler = Factory('Assembler')
+constructor = Factory('Constructor')
+drill = Factory('Drill')
+smeltery = Factory('Smeltery')
--- backend/information/products.py
+++ backend/information/products.py
@@ -1,6 +1,7 @@
 from backend.product import Product
 from backend.information import factories
 
+# Rise Of Industry
 water = Product('Water', 15, 1, factories.water_pump, {})
 wheat = Product('Wheat', 30, 2, factories.crop_farm, {water: 1})
 milk = Product('Milk', 30, 1, factories.livestock_farm, {water: 1, wheat: 1})
@@ -12,3 +13,19 @@ dough = Product('Dough', 30, 2, factories.food_factory, {flour: 1, water: 1})
 pizza = Product('Pizza', 30, 2, factories.food_factory, {cheese: 1, dough: 1, vegetables: 1})
 
 leather = Product('Leather', 30, 1, factories.livestock_farm, {water: 1, wheat: 1})
+
+# satisfactory
+iron_ore = Product('Iron ore', 1 / 60, 1, factories.drill, {})
+
+steel_ingot = Product("Steel ingot", 1 / 30, 1, factories.smeltery, {iron_ore: 1})
+
+steel_plate = Product('Steel plate', 1 / 10, 2, factories.constructor, {steel_ingot: 3})
+steel_rod = Product('Steel rod', 1 / 15, 1, factories.constructor, {steel_ingot: 1})
+screws = Product('Screws', 1 / 10, 4, factories.constructor, {steel_rod: 1})
+
+reinforced_steel_plate = Product('Reinforced_steel_plate', 1 / 5, 1, factories.assembler, {steel_plate: 6, screws: 12})
+reinforced_steel_plate_alt = Product("Reinforced steel plate alternative", 1 / 5, 3, factories.assembler,
+                                     {steel_plate: 18, screws: 50})
+rotor = Product('Rotor', 1 / 4, 1, factories.assembler, {steel_rod: 5, screws: 25})
+
+smart_cover = Product('Smart cover', 1/2, 1, factories.assembler, {reinforced_steel_plate: 1, rotor: 1})
